3ed985663e2c48e35993426ac485f6cc377012ae
pathoummao <>
2024-01-16 18:28:07 +0700

    - Vocabulary page

---
+++ src/views/home/services/data.js
@@ -0,0 +1,14 @@
+export const lesson = [
+  {
+    lesson: "1",
+    title: "Introduction",
+  },
+  {
+    lesson: "2",
+    title: "Time",
+  },
+  {
+    lesson: "3",
+    title: "Outfit",
+  },
+];
